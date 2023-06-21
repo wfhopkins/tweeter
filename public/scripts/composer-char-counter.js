82d4741cbc3dd4194611ff3c5d1f90c25fb7ced2
wfhopkins <will.hop@gmail.com>
2023-06-20 23:28:15 -0700

    counter behaves correctly with red neg

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,36 @@
+// $(document).ready(function() {
+//   console.log("Ready")
+  
+//   $("#tweet-text").on("input", function() {
+//     let inputLength = $(this).val().length;
+//     console.log(inputLength);
+//     let charsLeft = 140 - inputLength;
+
+//     if (charsLeft < 0) {
+//       $(".counter").addClass("exceeded");
+//     } else {
+//       $(".counter").removeClass("exceeded")
+//     }
+
+//     let $counter = $(this).closest('.container').find('.counter');
+//     $counter.text(charsLeft);
+//   });
+// });
+
+$(document).ready(function() {
+  console.log("Ready");
+
+  $("#tweet-text").on("input", function() {
+    let inputLength = $(this).val().length;
+    console.log(inputLength);
+    let charsLeft = 140 - inputLength;
+
+    if (charsLeft < 0) {
+      $(this).closest('.container').find('.counter').addClass("exceeded");
+    } else {
+      $(this).closest('.container').find('.counter').removeClass("exceeded");
+    }
+
+    $(this).closest('.container').find('.counter').text(charsLeft);
+  });
+});
