65d1595e344977d7eea4a8b638721e7a1a14efb8
wfhopkins <will.hop@gmail.com>
2023-06-21 17:58:28 -0700

    tweet template filled with dummy user data. css on tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+const createTweetElement = (tweetData) => {
+  const $tweet = $(`
+  
+    <article class="tweet">
+      <header>
+        <span class="user-avi"><img src="${tweetData.user.avatars}"></span><span class="user-name">${tweetData.user.name}</span>
+        <span class="handle">${tweetData.user.handle}</span>
+      </header>
+      <p class="tweet-content">${tweetData.content.text}</p>
+      <hr/>
+      <footer>
+        <span class="days">${tweetData.created_at}</span><span class="icons">
+          <i class="fa-solid fa-flag flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet retweet"></i>
+          <i class="fa-solid fa-heart heart"></i>
+        </span>
+      </footer>
+    </article>
+  `);
+
+  return $tweet;
+}
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+// doc.ready check before function calls
+$(()=> {
+  const $tweet = createTweetElement(tweetData);
+  
+  console.log($tweet);
+  console.log($(".tweets-container"))
+  $(".tweets-container").append($tweet);
+});
